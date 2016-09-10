850a3786a85eeb0eb4b19b918f26edb5331cae6c
Barrie Hadfield <barrie@workshare.com>
2016-09-10 12:11:57 +0100

    fix

--- config.rb
+++ config.rb
@@ -70,7 +70,7 @@ configure :build do
   activate :relative_assets
   set :relative_links, true
   set :site_url, "/reactrb.org"
-  set :http_prefix, '/reactrb.org'
+  # set :http_prefix, '/reactrb.org'
 
 end
 
