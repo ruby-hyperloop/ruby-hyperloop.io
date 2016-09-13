22b412b4a419d0f9d3ed5ece9236d56d7e84afbf
Barrie Hadfield <barrie@workshare.com>
2016-09-13 13:48:03 +0100

    blog paginate

--- config.rb
+++ config.rb
@@ -37,6 +37,8 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
 
+  blog.paginate = true
+
   # blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
   # blog.sources = "{year}-{month}-{day}-{title}.html"
