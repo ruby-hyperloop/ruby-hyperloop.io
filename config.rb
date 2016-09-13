cc5e092a551ef2a31dea59ee843ca200a9b3e806
Barrie Hadfield <barrie@workshare.com>
2016-09-13 15:55:32 +0100

    trying relative true

--- config.rb
+++ config.rb
@@ -83,7 +83,7 @@ configure :build do
   # activate :relative_assets
   # set :relative_links, true
   # set :site_url, "/reactrb.org"
-  set :http_prefix, '/reactrb.org' # enableing this stops sytlesheets working
+  set :http_prefix, '/reactrb.org' 
 
 end
 
